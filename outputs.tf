d1acb6a9a6ccf8ccaa788a89139dd00e5f6be644
Ryan Ryke <ryan@itsacloudlife.com>
2021-10-17 12:23:30 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,7 @@ output "private_subnets" {
 output "public_subnets" {
   value = join(", ", module.dynamic_subnets.public_subnet_ids)
 }
+
+output "route_table_ids" {
+  value = concat(module.dynamic_subnets.private_route_table_ids, module.dynamic_subnets.public_route_table_ids)
+}
