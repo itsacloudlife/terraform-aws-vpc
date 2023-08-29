bcfb4196cc4dcbac0939d19f507ae1b95e88efdd
BeardedGamingHero <pstagner@users.noreply.github.com>
2023-08-29 14:14:39 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -11,9 +11,9 @@ output "public_subnets" {
   value = join(", ", module.dynamic_subnets.public_subnet_ids)
 }
 
-output "route_table_ids" {
+/* output "route_table_ids" {
   value = concat(module.dynamic_subnets.private_route_table_ids, module.dynamic_subnets.public_route_table_ids)
-}
+} */
 
 output "igw_id" {
   value       = module.vpc.igw_id
