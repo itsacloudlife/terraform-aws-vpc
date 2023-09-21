318bd4473f69e14edd78a8f7c2d46432c35cfc92
David Thomson <adrnalnrsh@gmail.com>
2023-09-21 14:42:25 -0300

    testing list of cidrs for dynamic module

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ module "dynamic_subnets" {
   availability_zones = var.availability_zones
   vpc_id             = module.vpc.vpc_id
   igw_id             = module.vpc.igw_id
-  ipv4_cidr_block    = var.cidr_block
+  ipv4_cidr_block    = [var.cidr_block]
 }
 
 module "flow_logs" {
